9c003df2d05b4e3d23dbbdd38027a70ce390c63f
KOITT <KOITT@KOITT-PC>
2018-05-28 21:13:23 +0900

    예제 3-1부터 3-10까지

---
+++ ch03/src/OperatorEx03.java
@@ -0,0 +1,9 @@
+
+public class OperatorEx03 {
+	public static void main(String[] args) {
+		int i=5, j=5;
+		System.out.println(i++);	//변수 i먼저 사용하기 때문에 5출력
+		System.out.println(++j);	// 1 증가하고 변수 j 사용하기 때문에 6 출력
+		System.out.println("i = " + i + ", j = " +j);
+	}
+}
