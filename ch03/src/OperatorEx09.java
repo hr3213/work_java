81a89922da41af3a27ce5ba06f03edc05ede450f
KOITT <KOITT@KOITT-PC>
2018-05-28 21:32:42 +0900

    ch09 주석포함 ch11완성

--- ch03/src/OperatorEx09.java
+++ ch03/src/OperatorEx09.java
@@ -1,10 +1,10 @@
 
 public class OperatorEx09 {
 	public static void main(String[] args) {
-		long a = 1_000_000 * 1_000_000;
-		long b = 1_000_000L * 1_000_000L;
+		long a = 1_000_000 * 1_000_000;		//int형 * int형 = int형
+		long b = 1_000_000L * 1_000_000L;	//int형 * int형 = long형
 		
-		System.out.println("a="+a);
-		System.out.println("b="+b);
+		System.out.println("a="+a);	// 비정상 출력
+		System.out.println("b="+b);	// 정상 출력
 	}
 }
